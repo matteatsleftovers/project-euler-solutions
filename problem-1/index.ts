9529635351480c7386f41079c9f03d8155983b93
Matt <matteatsleftovers@gmail.com>
2023-08-22 14:31:57 -0400

    Extract to function with default parameter values

--- problem-1/index.ts
+++ problem-1/index.ts
@@ -1,9 +1,15 @@
-const upperLimit = 1000;
-let sum = 0;
-for (let i = 0; i < upperLimit; i++) {
-  if (i % 3 == 0 || i % 5 == 0) {
-    sum += i;
+function findSumOfMultiples(
+  upperLimit: number = 1000,
+  divisor1: number = 3,
+  divisor2: number = 5
+) {
+  let sum = 0;
+  for (let i = 0; i < upperLimit; i++) {
+    if (i % divisor1 == 0 || i % divisor2 == 0) {
+      sum += i;
+    }
   }
+  return sum;
 }
 
-console.log(`Here is an answer: ${sum}`);
+console.log(`Here is an answer: ${findSumOfMultiples()}`);
