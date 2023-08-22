3e1164c10ec9525042fce6b7b3e86bdf8cc28dbc
Matt <matteatsleftovers@gmail.com>
2023-08-22 15:25:47 -0400

    Create problem-2 directory with README and solution

---
+++ problem-2/index.ts
@@ -0,0 +1,16 @@
+function fibonacci(n: number) {
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function findSumOfEvenFibonaccis(upperLimit: number = 4_000_000) {
+  let sum = 0;
+  for (let i = 0; fibonacci(i) < upperLimit; i++) {
+    const fibonacciTerm = fibonacci(i);
+    if (fibonacciTerm % 2 == 0) sum += fibonacciTerm;
+  }
+  return sum;
+}
+
+console.log(`Here is an answer: ${findSumOfEvenFibonaccis()}`);
