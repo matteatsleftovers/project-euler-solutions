b3601fa2bafbb071eca20d5be5ac522041a37f94
Matt <matteatsleftovers@gmail.com>
2023-08-22 14:15:13 -0400

    Author solution to problem 1

--- problem-1/index.ts
+++ problem-1/index.ts
@@ -1,1 +1,9 @@
-console.log("This is not a solution to the problem.");
+const upperLimit = 1000;
+let sum = 0;
+for (let i = 0; i < upperLimit; i++) {
+  if (i % 3 == 0 || i % 5 == 0) {
+    sum += i;
+  }
+}
+
+console.log(`Here is an answer: ${sum}`);
